9580cfe45a1e4a069d3524b847bf6333e0bbb73f
ramirojacob7289-lang <ramirojacob7289@gmail.com>
2026-04-05 21:20:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <!DOCTYPE HTML>
 <html>
+	<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5018668,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5018668&101" alt="web log free" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
